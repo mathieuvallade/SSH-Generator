9d64db5dcca0baa4f49087b76d91562e881cfe3a
Mathieu <freezorce@linux.home>
2023-11-09 00:39:51 +0100

    update V1 SSH-Generator

--- main.py
+++ main.py
@@ -7,11 +7,22 @@ clear_terminal()
 
 print("Veuillez entrer le type de clef que vous souhaitez utiliser : ")
 key_type = input()
+
+while True:
+    print("Veuillez entrer le type de clé que vous souhaitez utiliser : ")
+    key_type = input()
+
+    if key_type not in ['rsa', 'dsa', 'ecdsa']:
+        print("Type de clé non valide. Veuillez entrer 'rsa', 'dsa' ou 'ecdsa'.")
+    else:
+        break
+
+print(f"Vous avez choisi le type de clé : {key_type}")
+
 print("Veuillez entrer en chiffre en combien de bit vous souhaitez encoder votre clef : ")
 key_size = input()
 print("Veuillez entrer le nom que vous souhaitez donner à votre clef : ")
 key_name = input()
 
-print("Voici la ligne de commande à utiliser :\n")
+print("Voici la ligne de commande à utiliser :")
 print(f"ssh-keygen -t {key_type} -b {key_size} -f /home/freezorce/.ssh/{key_name}")
-
