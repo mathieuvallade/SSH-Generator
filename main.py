3dbc58ea5fd75a85348370f6ed36bf9c2ef2021f
Mathieu Vallade <mathieuvallade3305@gmail.com>
2023-11-09 16:16:55 +0100

    Update V1

--- main.py
+++ main.py
@@ -6,8 +6,13 @@ print("En combien de bit voulez vous encoder votre clé ? : ")
 key_size = input()
 print(f"Vous avez encoder votre clé en {key_size} bit")
 
+print("Quel est votre nom de machine ? :")
+key_files = input()
+print(f"Votre nom de machine est {key_size}")
+
 print("Comment souhaitez vous appeller votre clé ? : ")
 key_name = input()
+print(f"Votre nom de machine est {key_name}")
 
 print("Voici la ligne de commande à utiliser :")
-print(f"ssh-keygen -t {key_type} -b {key_size} -f /home/freezorce/.ssh/{key_name}")
+print(f"ssh-keygen -t {key_type} -b {key_size} -f /home/{key_files}/.ssh/{key_name}")
