1739f68273dfac3307ce9bef608ae4568fa31365
Mathieu Vallade <mathieuvallade3305@gmail.com>
2023-11-20 15:46:14 +0100

    update V4

---
+++ key.py
@@ -0,0 +1,20 @@
+from function import *
+
+
+class RSAKey:
+    
+    def __init__(self, key_directory):
+        pass
+    def get_key_size():
+        pass
+    def generate_key():
+        pass
+    def print_key():
+        pass
+        
+# class DSAKey:
+
+# class ECDSAKey:
+
+# class EDDSAKey:
+
